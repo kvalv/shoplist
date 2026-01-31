cfea751aaaec9926dded2573d21017cd065b63f1
mikael kvalvaer <mikaelkvalvaer@gmail.com>
2026-01-31 23:39:55 +0100

    cron: first test case pass

--- cron/cron.go
+++ cron/cron.go
@@ -9,20 +9,129 @@ import (
 )
 
 type schedule struct {
-	Minute  pattern
-	Hour    pattern
-	Day     pattern
-	Month   pattern
-	Weekday pattern
+	Minute  filterer
+	Hour    filterer
+	Day     filterer
+	Month   filterer
+	Weekday filterer
 }
 
 func (s schedule) NextExecution(ref time.Time) time.Time {
-	want, _ := time.Parse(time.DateTime, "2000-01-02 04:05")
-	return want
+	ch := make(chan time.Time, 10)
+	result := s.Minute.filter(ch)
+	result = s.Hour.filter(ch)
+	result = s.Day.filter(ch)
+	result = s.Weekday.filter(ch)
 
+	for i := 0; ; i++ {
+		candidate := ref.Add(time.Minute * time.Duration(i))
+		select {
+		case ts := <-result:
+			fmt.Printf("got ts %s\n", ts)
+			return ts
+		case ch <- candidate:
+		}
+	}
 }
 
 type pattern string
+
+func (p pattern) values(start, end int) []int {
+	var res []int
+	if p == "*" {
+		for i := start; i < end; i++ {
+			res = append(res, i)
+		}
+	}
+	if p == "20-40" {
+		for i := 20; i <= 40; i++ {
+			res = append(res, i)
+		}
+	}
+	return res
+}
+
+type filterer interface {
+	filter(ch <-chan time.Time) chan time.Time
+}
+
+type minute pattern
+
+func (m minute) filter(ch <-chan time.Time) chan time.Time {
+	out := make(chan time.Time, 10)
+	go func() {
+		defer close(out)
+		for t := range ch {
+			for _, d := range pattern(m).values(0, 60) {
+				out <- withMinute(t, d)
+			}
+		}
+	}()
+	return out
+}
+
+type hour pattern
+
+func (h hour) filter(ch <-chan time.Time) chan time.Time {
+	out := make(chan time.Time, 10)
+	go func() {
+		defer close(out)
+		for t := range ch {
+			for _, d := range pattern(h).values(0, 24) {
+				out <- withHour(t, d)
+			}
+		}
+	}()
+	return out
+}
+
+type day pattern
+
+func (h day) filter(ch <-chan time.Time) chan time.Time {
+	out := make(chan time.Time, 10)
+	go func() {
+		defer close(out)
+		for t := range ch {
+			out <- t // TODO
+			// for _, d := range pattern(h).values(0, 24) {
+			// 	out <- withHour(t, d)
+			// }
+		}
+	}()
+	return out
+}
+
+type month pattern
+
+func (h month) filter(ch <-chan time.Time) chan time.Time {
+	out := make(chan time.Time, 10)
+	go func() {
+		defer close(out)
+		for t := range ch {
+			out <- t // TODO
+			// for _, d := range pattern(h).values(0, 24) {
+			// 	out <- withHour(t, d)
+			// }
+		}
+	}()
+	return out
+}
+
+type weekday pattern
+
+func (h weekday) filter(ch <-chan time.Time) chan time.Time {
+	out := make(chan time.Time, 10)
+	go func() {
+		defer close(out)
+		for t := range ch {
+			out <- t // TODO
+			// for _, d := range pattern(h).values(0, 24) {
+			// 	out <- withHour(t, d)
+			// }
+		}
+	}()
+	return out
+}
 
 func NewSchedule(pat string) (*schedule, error) {
 	parts := strings.Split(pat, " ")
@@ -30,11 +139,11 @@ func NewSchedule(pat string) (*schedule, error) {
 		return nil, fmt.Errorf("wrong number of parts: expected 5, got %d", len(parts))
 	}
 	s := schedule{
-		Minute:  pattern(parts[0]),
-		Hour:    pattern(parts[1]),
-		Day:     pattern(parts[2]),
-		Month:   pattern(parts[3]),
-		Weekday: pattern(parts[4]),
+		Minute:  minute(parts[0]),
+		Hour:    hour(parts[1]),
+		Day:     day(parts[2]),
+		Month:   month(parts[3]),
+		Weekday: weekday(parts[4]),
 	}
 
 	return &s, nil
@@ -65,4 +174,28 @@ func (c *cron) Register(
 	f Callback,
 ) Job {
 	panic("todo")
+}
+
+func withMinute(t time.Time, m int) time.Time {
+	return time.Date(
+		t.Year(), t.Month(), t.Day(),
+		t.Hour(), m, t.Second(), t.Nanosecond(),
+		t.Location(),
+	)
+}
+
+func withHour(t time.Time, h int) time.Time {
+	return time.Date(
+		t.Year(), t.Month(), t.Day(),
+		h, t.Minute(), t.Second(), t.Nanosecond(),
+		t.Location(),
+	)
+}
+
+func withDay(t time.Time, d int) time.Time {
+	return time.Date(
+		t.Year(), t.Month(), d,
+		t.Hour(), t.Minute(), t.Second(), t.Nanosecond(),
+		t.Location(),
+	)
 }
--- cron/cron_test.go
+++ cron/cron_test.go
@@ -2,26 +2,46 @@ package cron
 
 import (
 	"testing"
-	"testing/synctest"
 	"time"
 )
 
 func TestSchedule(t *testing.T) {
-	synctest.Test(t, func(t *testing.T) {
-		// inside here, the date is 2000-01-01, so it's a nice reference point
+	// synctest.Test(t, func(t *testing.T) {
+	// inside here, the date is 2000-01-01, so it's a nice reference point
 
-		// 23 0-20/2 * * *
+	ref, _ := time.Parse(time.DateOnly, "2000-01-01")
+
+	cases := []struct {
+		input string
+		want  string // yyyy-mm-dd hh:mm:ss
+	}{
+		{
+			input: "* * * * *",
+			want:  "2000-01-01 00:00:00",
+		},
+		// {
+		// 	input: "5 4 * * sun",
+		// 	want:  "2000-01-02 04:05",
+		// },
+	}
+
+	for _, tc := range cases {
+		want, err := time.Parse(time.DateTime, tc.want)
+		if err != nil {
+			t.Fatalf("test setup error: invalid date: %s", err)
+		}
 
 		// at 04:05 on Sunday
-		sched, err := NewSchedule("5 4 * * sun")
+		sched, err := NewSchedule(tc.input)
 		if err != nil {
 			t.Fatalf("failed to make sched: %v", err)
 		}
-		got := sched.NextExecution(time.Now())
-		want, _ := time.Parse(time.DateTime, "2000-01-02 04:05")
+		t.Logf("ref is %s", ref)
+		got := sched.NextExecution(ref)
+		t.Logf("got is %s -- right bfore tunrt", got)
 		if got != want {
 			t.Fatalf("mismatch: expected %s, got %s", got.Format(time.DateTime), want.Format(time.DateTime))
 		}
-
-	})
+	}
+	// })
 }
